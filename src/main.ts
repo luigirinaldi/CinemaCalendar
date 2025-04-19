ecf2b3cc4123bdf66f733555d96a4b6b8429d714
luigirinaldi <luigirinaldi@users.noreply.github.com>
2025-04-19 19:22:51 +0200

    auto formatting

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 
-document.addEventListener('DOMContentLoaded', function() {
-    console.log("Hello World!")
-    let calendarEl : HTMLElement = document.getElementById('calendar')!;
-    let calendar = new Calendar(calendarEl, {
-        plugins: [ dayGridPlugin ],
-        initialView: 'dayGridMonth'
-    });
-    calendar.render();
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('Hello World!');
+  let calendarEl: HTMLElement = document.getElementById('calendar')!;
+  let calendar = new Calendar(calendarEl, {
+    plugins: [dayGridPlugin],
+    initialView: 'dayGridMonth',
   });
+  calendar.render();
+});

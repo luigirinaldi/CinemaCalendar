67d58daa79aeaf50f4110fef903bdd5b57aec817
Dj-Frixz <67787064+Dj-Frixz@users.noreply.github.com>
2025-04-30 00:38:49 +0200

    Rex-cinema (#10)
    added Rex cinema

---
+++ scripts/rexCinema.ts
@@ -0,0 +1,29 @@
+import type { CinemaShowing } from '../src/types';
+
+export async function scraper(): Promise<CinemaShowing[]> {
+  const response = await fetch(`https://www.cinemarex.it/pages/rexJsonCompact.php`);
+
+  console.log(
+    'The request to rexcinema API gave response code: ',
+    response.status
+  );
+  if (response.status !== 200) {
+    return []; // This returns an empty list of movies so the getData script won't be stopped in case of an error
+  }
+  const data = await response.json();
+
+  return [
+    {
+      cinema: 'RexCinema',
+      location: 'Padova',
+      showings: data.titoli.flatMap((film) =>
+        film.eventi.map((show) => ({
+          name: film.titolo,
+          tmdbId: show.id_cinebot,
+          startTime: show.inizio,
+          duration: film.durata,
+        }))
+      )
+    }
+  ];
+}

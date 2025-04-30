ca2e992d6a472c5adcd1b0cade28d1882c906296
luigirinaldi <luigirinaldi@users.noreply.github.com>
2025-04-30 22:45:42 +0100

    quicker data insertion

--- scripts/main.ts
+++ scripts/main.ts
@@ -17,8 +17,6 @@ const scrapers: ScraperFunction[] = [
   RexScraper,
 ];
 
-
-
 async function writeFile(data, filename: string) {
   fs.writeFile(filename, JSON.stringify(data), (err: any) => {
     if (err) {
@@ -43,7 +41,7 @@ async function main() {
   db.prepare(
     `CREATE TABLE IF NOT EXISTS films (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        title TEXT NOT NULL,
+        title TEXT UNIQUE NOT NULL,
         duration_minutes INTEGER,
         tmdb_id INTEGER)`
   ).run();
@@ -61,54 +59,45 @@ async function main() {
         FOREIGN KEY (film_id) REFERENCES films(id))`
   ).run();
 
-  const cinemas: string[] = (await Promise.all(scrapers.map( async fun => {
-    const result = await fun();
-    const insertCinema = db.prepare('INSERT INTO cinemas (name, location) VALUES (@name, @location)');
-    return Object.entries(result).map(([_, cinema]) => {
-      console.log(cinema.cinema);
+  await Promise.all(
+    scrapers.map(async (fun) => {
+      const result = await fun();
+      const insertCinema = db.prepare(
+        'INSERT INTO cinemas (name, location) VALUES (@name, @location)'
+      );
+      const getFilmId = db.prepare('SELECT id FROM films WHERE title=@title');
+      const insertFilm = db.prepare(
+        'INSERT OR IGNORE INTO films (title, duration_minutes, tmdb_id) VALUES (?, ? ,?)'
+      );
+      const insertFilmShowing = db.prepare(
+        'INSERT INTO film_showings (cinema_id, film_id, start_time, end_time) VALUES (?,?,?,?)'
+      );
+      Object.entries(result).forEach(([_, cinema]) => {
+        console.log(cinema.cinema);
 
-      const {changes, lastInsertRowid: cinema_id} = insertCinema.run({name: cinema.cinema, location: cinema.location})
-      // console.log(db.prepare("SELECT * from cinemas where location='Padova'").all());
-      cinema.showings.forEach(film => {
-        const checkFilm = db.prepare('SELECT * FROM films WHERE title=@title');
-        const matchingFilm = checkFilm.all({'title':film.name});
-        const film_id = (matchingFilm.length == 0) 
-                      ? 
-                      db.prepare('INSERT INTO films (title, duration_minutes, tmdb_id) VALUES (?, ? ,?)')
-                        .run(film.name, film.duration, film.tmdbId).lastInsertRowid 
-                      :
-                      matchingFilm[0].id;
-          // console.log(info);
-        
-          // console.log(`${film.name} in db`);
-          // console.log(matchingFilm);
-        // }
-
-        const insertFilmShowing = db.prepare('INSERT INTO film_showings (cinema_id, film_id, start_time, end_time) VALUES (?,?,?,?)');
-        insertFilmShowing.run(cinema_id, film_id, film.startTime, film.endTime);
-
-      })
-
-      return cinema.cinema
+        const { changes, lastInsertRowid: cinema_id } = insertCinema.run({
+          name: cinema.cinema,
+          location: cinema.location,
+        });
+        const insertAll = db.transaction(() => {
+          cinema.showings.forEach((film) => {
+            insertFilm.run(film.name, film.duration, film.tmdbId);
+            const film_id = getFilmId.get({ title: film.name });
+            insertFilmShowing.run(
+              cinema_id,
+              film_id.id,
+              film.startTime,
+              film.endTime
+            );
+          });
+        });
+        insertAll();
+        console.log(`Inserted movies for ${cinema.cinema}`);
+      });
     })
-  }))).flat();
+  );
 
-  console.log(cinemas);
-    // for (const [name, scraper] of Object.entries(scrapers)) {
-    //   const result = await scraper();
-    //   Object.entries(result).forEach(([_, cinema]) => {
-    //     console.log(cinema.cinema)
-    //   })
-      // for (const [_, cinema] of Object.entries(result)) {
-        //   cinemas.push(cinema.cinema);
-        //   await writeFile(cinema.showings, `./public/data/${cinema.cinema}.json`);
-        // }
-      // }
-  //   }
-  // );
-      
   db.close();
-  // await writeFile(cinemas, './public/data/cinemas.json');
 }
 
 main();

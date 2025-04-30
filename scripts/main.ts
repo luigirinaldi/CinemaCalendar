920ad613f8ed76cba26abcae7f1e13f0b1c8ae98
luigirinaldi <luigirinaldi@users.noreply.github.com>
2025-04-30 17:19:38 +0100

    start db

--- scripts/main.ts
+++ scripts/main.ts
@@ -6,6 +6,9 @@ import { scraper as LuxPadovaScraper } from './luxPadova';
 import { scraper as PrinceScraper } from './princeCharlesCinema';
 import { scraper as RexScraper } from './rexCinema';
 
+import sqlite3 from "sqlite3";
+import { execute } from './sql';
+
 const scrapers: Record<string, ScraperFunction> = {
   RegentScraper,
   theSpaceLimenaScraper,
@@ -25,16 +28,62 @@ async function writeFile(data, filename: string) {
 }
 
 async function main() {
-  let cinemas: string[] = [];
-  for (const [name, scraper] of Object.entries(scrapers)) {
-    const result = await scraper();
-    for (const [_, cinema] of Object.entries(result)) {
-      cinemas.push(cinema.cinema);
-      await writeFile(cinema.showings, `./public/data/${cinema.cinema}.json`);
-    }
+  const db = new sqlite3.Database('./my.db');
+
+  // Make cinema table
+  try {
+    await execute(
+      db,
+      `CREATE TABLE IF NOT EXISTS cinemas (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        location TEXT NOT NULL)`
+    );
+  } catch (error) {
+    console.log(error);
+  }
+  // Make film table
+  try {
+    await execute(
+      db,
+      `CREATE TABLE IF NOT EXISTS films (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        duration_minutes INTEGER,
+        tmdb_id INTEGER)`
+    );
+  } catch (error) {
+    console.log(error);
   }
 
-  await writeFile(cinemas, './public/data/cinemas.json');
+  // Make showings table
+  try {
+    await execute(
+      db,
+      `CREATE TABLE IF NOT EXISTS film_showings (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        cinema_id INTEGER NOT NULL,
+        film_id INTEGER NOT NULL,
+        showing_time TEXT NOT NULL,
+
+        FOREIGN KEY (cinema_id) REFERENCES cinemas(id),
+        FOREIGN KEY (film_id) REFERENCES films(id))`
+    );
+  } catch (error) {
+    console.log(error);
+  }
+
+  db.close()
+  // let cinemas: string[] = [];
+  // for (const [name, scraper] of Object.entries(scrapers)) {
+  //   const result = await scraper();
+  //   for (const [_, cinema] of Object.entries(result)) {
+  //     cinemas.push(cinema.cinema);
+  //     await writeFile(cinema.showings, `./public/data/${cinema.cinema}.json`);
+  //   }
+  // }
+
+  // await writeFile(cinemas, './public/data/cinemas.json');
 }
 
 main();
---
+++ scripts/sql.ts
@@ -0,0 +1,11 @@
+
+
+export const execute = async (db, sql) => {
+    return new Promise((resolve, reject) => {
+      db.exec(sql, (err) => {
+        if (err) reject(err);
+        resolve();
+      });
+    });
+  };
+  

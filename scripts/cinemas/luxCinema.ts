54d307b4720d73833b00ab7eb830acfce0eaa916
Dj Frixz <gmsccntdenmark@gmail.com>
2025-05-01 19:49:56 +0200

    ics parser added to utils

--- scripts/cinemas/luxCinema.ts
+++ scripts/cinemas/luxCinema.ts
@@ -1,36 +1,6 @@
 import ICAL from 'ical.js';
+import { fetchAndParseICS } from '../utils';
 import type { CinemaShowing, FilmShowing } from '../../src/types';
-
-async function fetchAndParseICS(
-  url: string,
-  parseEvent: Function,
-  filter: boolean = false
-): Promise<FilmShowing[]> {
-  const res = await fetch(url);
-  const icsText = await res.text();
-
-  const jcalData = ICAL.parse(icsText);
-  const comp = new ICAL.Component(jcalData);
-  let vevents = comp.getAllSubcomponents('vevent');
-
-  // filter out past showings
-  if (filter) {
-    const today = new Date();
-    today.setHours(0, 0, 0, 0); // zero out time to get beginning of today
-    console.log(today);
-    vevents = vevents.filter((vevent) => {
-      const event = new ICAL.Event(vevent);
-      return event.startDate.toJSDate() >= today;
-    });
-  }
-
-  const events: FilmShowing[] = vevents.map((vevent) => {
-    const event = new ICAL.Event(vevent);
-    return parseEvent(event, vevent);
-  });
-
-  return events;
-}
 
 /**
  * Choose how to parse the event data into FilmShowing objects.

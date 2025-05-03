1a2fa3d9c877a5c6face90440cb938e3bc51eabd
luigirinaldi <luigirinaldi@users.noreply.github.com>
2025-05-03 16:11:36 +0100

    working cine lumiere parsing

--- scripts/cinemas/instituteFrancaisCinema.ts
+++ scripts/cinemas/instituteFrancaisCinema.ts
@@ -1,6 +1,8 @@
-import { method } from 'happy-dom/lib/PropertySymbol.js';
 import { CinemaShowing, FilmShowing } from '../../src/types';
 import { parse } from 'node-html-parser';
+
+const CINEMA_NAME = 'Ciné Lumière';
+const LOG_PREFIX = '[' + CINEMA_NAME + ']';
 
 // function that performs type restriction, so that the type coming out doesn't have any undefined things
 function hasNoUndefined<T extends Record<string, any>>(
@@ -10,16 +12,6 @@ function hasNoUndefined<T extends Record<string, any>>(
 }
 
 export async function scraper(): Promise<CinemaShowing[]> {
-  let body = {
-    type: '72',
-    period: 'any',
-    location: 'onsite',
-    'date-display': '',
-    date: '',
-  };
-
-  let headers = {};
-
   let response = await fetch(
     'https://www.institut-francais.org.uk/whats-on/?type=72&period=any&location=onsite',
     {
@@ -30,10 +22,6 @@ export async function scraper(): Promise<CinemaShowing[]> {
   let html = await response.text();
   const root = parse(html);
 
-  // 👇️ const result: CreateUserResponse
-  console.log(response);
-  console.log(root);
-
   // Select all article elements with a specific class
   const movies = root.querySelectorAll('article.card--film');
 
@@ -43,7 +31,7 @@ export async function scraper(): Promise<CinemaShowing[]> {
       .querySelector('div.card__information')
       ?.querySelector('div.card__metadata');
     const info = {
-      href: link?.getAttribute('href'),
+      url: link?.getAttribute('href'),
       title: link?.getAttribute('title'),
       tags: metadata
         ?.querySelector('div.card__terms')
@@ -55,6 +43,7 @@ export async function scraper(): Promise<CinemaShowing[]> {
       return [info];
     } else {
       console.warn(
+        LOG_PREFIX,
         'Failed to get some information for movie:',
         info,
         movie.toString()
@@ -63,60 +52,146 @@ export async function scraper(): Promise<CinemaShowing[]> {
     }
   });
 
-  console.log(movie_info);
-  console.log(movie_info.length);
+  // console.debug(LOG_PREFIX,`Obtained a total of ${movie_info.length} movies`);
 
-  const movie1 = await fetch(movie_info[0].href, { method: 'GET' });
-  const root1 = parse(await movie1.text());
+  const film_showings = (
+    await Promise.all(
+      movie_info.map(async (movie) => {
+        const movie_response = await fetch(movie.url, { method: 'GET' });
+        const parsed_page = parse(await movie_response.text());
 
-  console.log(root1.toString());
-  console.log(root1);
-  console.log(root1.getElementById('more-dates'));
-  console.log(
-    root1
-      .getElementById('more-dates')
-      ?.querySelector('table')
-      ?.querySelector('tbody')
-      ?.querySelectorAll('tr')
-      ?.map((showing) => {
-        return {
-          datetime: showing
-            .querySelectorAll('time')
-            ?.map((time) => time.getAttribute('datetime'))
-            .join('T'),
-          url: showing.querySelector('a')?.getAttribute('href'),
-          room:
-            showing
-              .querySelectorAll('td')
-              ?.flatMap((td) =>
-                td.childElementCount == 0 ? [td.innerHTML] : []
-              )[0] ?? undefined,
+        let showings = parsed_page
+          .getElementById('more-dates')
+          ?.querySelector('table')
+          ?.querySelector('tbody')
+          ?.querySelectorAll('tr')
+          ?.flatMap((showing) => {
+            const showing_out = {
+              datetime: showing
+                .querySelectorAll('time')
+                ?.map((time) => time.getAttribute('datetime'))
+                .join('T'),
+              url: showing.querySelector('a')?.getAttribute('href'),
+              room: (showing
+                .querySelectorAll('td')
+                ?.flatMap((td) =>
+                  td.childElementCount == 0 ? [td.innerHTML] : []
+                )[0] ?? null) as string | null,
+            };
+            if (hasNoUndefined(showing_out)) {
+              return [showing_out];
+            } else {
+              console.warn(
+                LOG_PREFIX,
+                `Failed to get some information for movie ${movie.title}:`,
+                showing_out,
+                showing.toString(),
+                movie
+              );
+              return [];
+            }
+          });
+
+        // the case where there is only one showing, hence the more-dates element doesn't appear
+        if (showings == undefined) {
+          const showing_html = parsed_page.querySelector(
+            'section.next-showing'
+          );
+          const showing_info = {
+            datetime: showing_html
+              ?.querySelectorAll('time')
+              ?.map((time) => time.getAttribute('datetime'))
+              .join('T'),
+            url: showing_html?.querySelector('a')?.getAttribute('href'),
+            room:
+              showing_html?.querySelector('span.location')?.innerHTML ?? null,
+          };
+          if (hasNoUndefined(showing_info)) {
+            showings = [showing_info];
+          } else {
+            console.warn(
+              LOG_PREFIX,
+              `Failed to get information for movie ${movie.title}:`,
+              showing_info,
+              showing_html?.toString(),
+              movie
+            );
+          }
+        }
+
+        const metadata_raw: Record<string, string> = Object.fromEntries(
+          parsed_page
+            .querySelector('ul.metadata')
+            ?.querySelectorAll('li')
+            .map((li) => {
+              // remove the ':' at the end of the string
+              const key_string = li
+                .querySelector('strong')
+                ?.innerText.trim()
+                ?.slice(0, -1);
+              const value_string = li.childNodes
+                .at(li.childNodes.length - 1)
+                ?.innerText.trim();
+              return [key_string, value_string];
+            })
+            // filter out any undefined values
+            .filter((val) => val.every((val) => val != undefined)) as string[][]
+        );
+
+        let duration: number | null = null;
+        if ('Duration' in metadata_raw) {
+          const match = metadata_raw['Duration'].match(/^(\d+)\s*min(s?)/);
+          if (match) {
+            // console.log(movie.title, 'duration:', duration );
+            duration = +match[1];
+          } else {
+            console.warn(
+              LOG_PREFIX,
+              `Couldn't get duration for ${movie.title}\n`,
+              metadata_raw['Duration'].toString(),
+              movie.url
+            );
+            duration = null;
+          }
+        } else {
+          console.warn(
+            LOG_PREFIX,
+            `No duration specified for ${movie.title}`,
+            movie.url
+          );
+          duration = null;
+        }
+
+        const metadata = {
+          duration: duration,
+          raw_meta: metadata_raw,
         };
-        // line.querySelectorAll('td').map((el) => el)
+
+        return {
+          ...movie,
+          ...metadata,
+          showings: showings,
+        };
       })
-  );
-  console.log(movie_info[0].title);
-  //   let movie_data: object = result['data']['showingsForDate']['data'];
+    )
+  ).filter((movie) => hasNoUndefined(movie));
 
-  //   let movie_info_out: Array<FilmShowing> = [];
+  // console.log(film_showings)
 
-  //   for (let [_key, movie] of Object.entries(movie_data)) {
-  //     let movie_info: FilmShowing = {
-  //       name: movie['movie']['name'],
-  //       tmdbId: movie['movie']['tmdbId'],
-  //       startTime: movie['time'],
-  //       duration: movie['movie']['duration'],
-  //       url: 'https://www.regentstreetcinema.com/checkout/showing/' + movie['id'],
-  //     };
-  //     movie_info_out.push(movie_info);
-  //   }
-  //   return [
-  //     {
-  //       cinema: 'RegentStreetCinema',
-  //       location: 'London',
-  //       showings: movie_info_out,
-  //     },
-  //   ];
+  return [
+    {
+      cinema: CINEMA_NAME,
+      location: 'London',
+      showings: film_showings.flatMap((film) => {
+        return film.showings.map((showings) => {
+          return {
+            name: film.title,
+            startTime: showings.datetime,
+            url: showings.url,
+            duration: film.duration ?? 0, // sometimes there are movies where the duration is not specified so have to put 0
+          } as FilmShowing;
+        });
+      }),
+    },
+  ];
 }
-
-scraper();

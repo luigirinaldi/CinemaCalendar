d17447c3e5d55741c0449de3f2464cf1911bc6ab
luigirinaldi <luigirinaldi@users.noreply.github.com>
2025-11-18 12:04:03 +0000

    add to metadata fetch

--- scripts/getTmdbId.ts
+++ scripts/getTmdbId.ts
@@ -1,10 +1,13 @@
 import { connectDB } from './database';
+import { updateLetterboxdMeta } from './letterboxd_meta';
 import { updateFilmMetadata } from './metadata';
 
 async function main() {
     const db = await connectDB();
 
     await updateFilmMetadata(db);
+
+    await updateLetterboxdMeta(db, true);
 
     await db.destroy();
 }

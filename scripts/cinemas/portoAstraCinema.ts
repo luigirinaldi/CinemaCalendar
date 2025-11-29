92c3839f79ae64b2f5e0b841ede80fbe8fd1cd32
Dj Frixz <gmsccntdenmark@gmail.com>
2025-11-29 15:27:06 +0100

    fixed Porto Astra datetimes (fix #77)

--- scripts/cinemas/portoAstraCinema.ts
+++ scripts/cinemas/portoAstraCinema.ts
@@ -1,90 +1,90 @@
 import type { Cinema, CinemaShowings, Showing } from '../types';
+import { setTimeZone } from '../utils';
 
 export interface APIResponse {
     userdata: Userdata;
-    DS:       Ds;
-}    
+    DS: Ds;
+}
 
 export interface Ds {
-    Scheduling:    Scheduling;
+    Scheduling: Scheduling;
     OptinRequired: boolean;
-    TicketoFound:  boolean;
-}    
+    TicketoFound: boolean;
+}
 
 export interface Scheduling {
-    LocalId:  number;
+    LocalId: number;
     LogoPath: string;
-    Events:   Event[];
-}    
+    Events: Event[];
+}
 
 export interface Event {
-    EventId:         number;
-    Title:           string;
-    OriginalTitle:   string;
-    Description:     string;
-    Actors:          string;
-    Type:            Type;
-    Is3D:            boolean;
-    Director:        string;
-    Duration:        string;
-    Picture:         string;
-    Year:            string;
-    Category:        string;
-    MovieId:         string;
-    TitleId:         string;
-    Properties:      string[];
+    EventId: number;
+    Title: string;
+    OriginalTitle: string;
+    Description: string;
+    Actors: string;
+    Type: Type;
+    Is3D: boolean;
+    Director: string;
+    Duration: string;
+    Picture: string;
+    Year: string;
+    Category: string;
+    MovieId: string;
+    TitleId: string;
+    Properties: string[];
     EventProperties: EventProperty[];
-    Days:            Day[];
-    IsStreaming:     boolean;
-}    
+    Days: Day[];
+    IsStreaming: boolean;
+}
 
 export interface Day {
     GroupedByScreen: boolean;
-    Performances:    Performance[];
-    Day:             Date;
-}    
+    Performances: Performance[];
+    Day: Date;
+}
 
 export interface Performance {
     PerformanceId: number;
-    Time:          string;
-    Screen:        Screen;
-    ScreenId:      number;
-    StartTime:     Date;
-    EndTime:       Date;
-    Duration:      number;
-}    
+    Time: string;
+    Screen: Screen;
+    ScreenId: number;
+    StartTime: Date;
+    EndTime: Date;
+    Duration: number;
+}
 
 export enum Screen {
-    Sala1 = "SALA 1",
-    Sala2 = "SALA 2",
-    Sala3 = "SALA 3",
-    Sala4 = "SALA 4",
-    Sala5 = "SALA 5",
-    Sala6 = "SALA 6",
-    Sala7 = "SALA 7",
-}    
+    Sala1 = 'SALA 1',
+    Sala2 = 'SALA 2',
+    Sala3 = 'SALA 3',
+    Sala4 = 'SALA 4',
+    Sala5 = 'SALA 5',
+    Sala6 = 'SALA 6',
+    Sala7 = 'SALA 7',
+}
 
 export interface EventProperty {
-    EventProperty:         string;
+    EventProperty: string;
     EventPropertyCategory: string;
-}    
+}
 
 export enum Type {
-    Cinema = "CINEMA",
-}    
+    Cinema = 'CINEMA',
+}
 
 export interface Userdata {
-    StatusOk:   boolean;
+    StatusOk: boolean;
     ReasonCode: number;
-    Message:    string;
+    Message: string;
     StackTrace: string;
-    Elapsed:    number;
-    StartTime:  Date;
-    EndTime:    Date;
-    TimeTrace:  string[];
-    Errors:     any[];
-}    
-
+    Elapsed: number;
+    StartTime: string; // Date?
+    EndTime: Date;
+    TimeTrace: string[];
+    Errors: any[];
+}
 
 // all info at https://secure.webtic.it/api/wtjsonservices.ashx?languageid=it&localid=5082&trackid=33&wtid=getLocalInfo
 const CINEMA: Cinema = {
@@ -94,7 +94,8 @@ const CINEMA: Cinema = {
         lat: '45.38365',
         lng: '11.8656',
     },
-    defaultLanguage: 'it-IT'
+    defaultLanguage: 'it-IT',
+    timezone: 'Europe/Rome',
 };
 
 const LOG_PREFIX = '[' + CINEMA.name + ']';
@@ -105,7 +106,16 @@ const IMAGE_BASE_URL = 'https://secure.webtic.it/angwt/';
 
 export async function scraper(): Promise<CinemaShowings> {
     const res = await fetch(API_URL);
+    if (!res.ok) {
+        console.warn(
+            `${LOG_PREFIX} The request to WebTic API gave response code: `,
+            res.status
+        );
+        return []; // This returns an empty list of movies so the getData script won't be stopped in case of an error
+    }
     const json = (await res.json()) as APIResponse;
+
+    const timezone = json.userdata.StartTime.match(/[+-]\d{2}:?\d{2}$/)?.[0];
 
     return [
         {
@@ -133,9 +143,12 @@ export async function scraper(): Promise<CinemaShowings> {
                     day.Performances.map(
                         (showing) =>
                             ({
-                                startTime: new Date(
-                                    showing.StartTime
-                                ).toISOString(),
+                                startTime: timezone
+                                    ? showing.StartTime + timezone
+                                    : setTimeZone(
+                                          showing.StartTime,
+                                          CINEMA.timezone
+                                      ),
                                 theatre: showing.Screen,
                                 bookingUrl: `http://www.webtic.it/mobile/?trackid=41&action=loadPerformance&localId=${json.DS.Scheduling.LocalId}&eventId=${event.EventId}&performanceId=${showing.PerformanceId}`,
                             }) as Showing
